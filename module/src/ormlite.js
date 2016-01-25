5d5efe46018685d21324c37295069419d42a1349
Nalar <thijstel@gmail.com>
2016-01-25 21:22:16 +0100

    Updated to latest version

--- module/src/ormlite.js
+++ module/src/ormlite.js
@@ -1,50 +1,51 @@
+// Define requires
 var pg = require('pg');
 
-//var connectionString = "postgres://postgres:sergtsop@localhost:5432/postgres";
+// Define empty vars for use later on
+var connectedClient;	// This will contain the client that is connected
+var doneClient;			// This will contain the done function of the client
 
-var connectedClient;
-var doneClient;
-
-function initialize(connectionString, onComplete){
+function initialize(connectionString, onComplete){					// Define function initialize
 	// Takes in connection string and connects 
 	// to the specified database
-	pg.connect(connectionString, function (err, client, done){
-		if(err){throw err};
-		connectedClient = client;
-		doneClient = done;
-		onComplete(client);
+	pg.connect(connectionString, function (err, client, done){		// Connect to the postgres database
+		if(err){throw err};											// Error handler
+		connectedClient = client;									// Define the connected client
+		doneClient = done;											// Define the done function
+		onComplete(client);											// Pass the client on to the callback
 	});
 };
 
-function getAll(tableName, onComplete){
+function getAll(tableName, onComplete){								// Define function getAll
 	// Takes in name of table and returns its
 	// contents as an array
-	var tableArray;
+	var tableArray;													// Create an empty var to hold the results
 	
-	connectedClient.query('select * from ' + tableName + ';', function (err, result){
-		if(err){throw err};
-		tableArray = result.rows;
-		onComplete(tableArray);
+	connectedClient.query('select * from ' + tableName + ';', function (err, result){	// Query the database for everything in table tableName
+		if(err){throw err};																// Error handler
+		tableArray = result.rows;														// Store the table's rows in tableArray
+		onComplete(tableArray);															// Pass on tableArray to the callback
 	});
 };
 
-function findByID(tableName, ID, onComplete){
+function findByID(tableName, ID, onComplete){						// Define function findByID
 	// Takes in two parameters: table name and ID
 	// and returns an object with a row in that
 	// table
 	
-	connectedClient.query('select * from ' + tableName + ' where id = ' + ID + ';', function (err, result){
-		if(err){throw err};
-		tableObject = result.rows;
-		onComplete(tableObject);
+	connectedClient.query('select * from ' + tableName + ' where id = ' + ID + ';', function (err, result){		// Query the database for ID in tableName
+		if(err){throw err};				// Error handler
+		tableObject = result.rows;		// Return the results to tableObject
+		onComplete(tableObject);		// Pass on tableObject to the callback
 	});
 };
 
-function deinitialize(){
-	doneClient();
-	pg.end();
+function deinitialize(){	// Define function de-initialize
+	doneClient();			// Call the done function
+	pg.end();				// End all pg connections
 };
 
+// Define the module's exports
 module.exports = {
 	initialize: initialize,
 	getAll: getAll,
--- testOrmLite.js
+++ testOrmLite.js
@@ -1,20 +1,20 @@
-var ormlite = require('./module/src/ormlite.js');
+var ormlite = require('./module/src/ormlite.js');								// Require orm-lite
 
-var connectionString = "postgres://postgres:sergtsop@localhost:5432/postgres";
+var connectionString = "postgres://postgres:sergtsop@localhost:5432/postgres";	// Define the connection string
 
-var allTableData;
-var specificData;
-var count = 0;
+var allTableData;	// Empty var
+var specificData;	// Empty var
+var count = 0;		// Count used to determine end
 
-ormlite.initialize(connectionString, function(connectedClient){
+ormlite.initialize(connectionString, function(connectedClient){			// Call ormlite.initialize to initialize database connection
 
-	ormlite.getAll('test_user', function(tableArray){
+	ormlite.getAll('test_user', function(tableArray){					// Get all entries in table test_user
 		console.log(tableArray);
 		allTableData = tableArray;
 		finishUp();
 	});
 
-	ormlite.findByID('test_user', 3, function(tableObject){
+	ormlite.findByID('test_user', 3, function(tableObject){				// Get entry with id 3 in table test_user
 		console.log(tableObject);
 		var specificData = tableObject;
 		finishUp();
@@ -24,7 +24,7 @@ ormlite.initialize(connectionString, function(connectedClient){
 		count++
 		if(count == 2){
 			console.log("Done!");
-			ormlite.deinitialize();
+			ormlite.deinitialize();										// Call the de-initialize function
 		}
 	}
 
